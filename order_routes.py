2133c8ba4960da7f85f9cc5c32e26493d71aba5b
anketmanoj <68798781+anketmanoj@users.noreply.github.com>
2022-09-03 13:12:00 +0900

    place order done

--- order_routes.py
+++ order_routes.py
@@ -1,10 +1,42 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi_jwt_auth import AuthJWT
+from database import SessionLocal, get_db
+from models import Users, Orders
+from schema import OrderModel
+from fastapi.encoders import jsonable_encoder
 
 order_router = APIRouter(
     prefix="/orders",
     tags=['Orders']
 )
 
-@order_router.get("/")
-async def hello():
-    return {"message" : "Hello, this order works"}
+@order_router.post("/", status_code= status.HTTP_201_CREATED)
+async def place_order(order: OrderModel, Authorize: AuthJWT = Depends(), db: SessionLocal = Depends(get_db)):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User is not authorized")
+
+    current_user = Authorize.get_jwt_subject()
+    user = db.query(Users).filter(Users.username == current_user).first()
+
+    new_order = Orders(
+        pizza_size= order.pizza_size,
+        quantity=order.quantity,
+    )
+
+    new_order.users = user
+
+    db.add(new_order)
+    db.commit()
+    db.refresh(new_order)
+
+    return new_order
+
+
+
+
+    
+        
+    
+    
--- schema.py
+++ schema.py
@@ -29,3 +29,20 @@ class Setting(BaseModel):
 class LoginUser(BaseModel):
     username: str
     password: str
+
+class OrderModel(BaseModel):
+    id : Optional[int]
+    quantity : int
+    order_status : Optional[str] = "PENDING"
+    pizza_size : Optional[str] = "SMALL"
+    flavour : Optional[str] = "pepperoni"
+    user_id : Optional[int] 
+
+    class Config:
+        orm_mode = True
+        schema_extra={
+            'example' : {
+                "quantity" : 1,
+                "pizza_size" : "SMALL"
+            }
+        }
